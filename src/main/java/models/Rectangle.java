b40f9c0a7660e6753a00e172e96f2dc99d0bf26b
Charles NDAYISABA <nccharles1@gmail.com>
2020-11-11 23:48:26 +0200

    initial

---
+++ src/main/java/models/Rectangle.java
@@ -0,0 +1,22 @@
+package models;
+
+public class Rectangle {
+    private int height;
+    private int width;
+
+    public Rectangle(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public int getHeight(){
+        return this.height;
+    }
+
+    public int getWidth(){
+        return this.width;
+    }
+    public boolean isSquare(){
+        return width==height;
+    }
+}
